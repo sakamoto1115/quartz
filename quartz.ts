b9500e69e839606f719ce0076d31a3ca976cb0db
sakamoto1115 <sakamoto1115neko@gmail.com>
2026-08-16 09:40:20 +0900

    デザインの修正

--- quartz.ts
+++ quartz.ts
@@ -1,7 +1,14 @@
 import { registerCondition } from "./quartz/plugins/loader/conditions"
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { componentRegistry } from "./quartz/components/registry"
 
 registerCondition("is-index", (props) => props.fileData.slug === "index")
+
+// 「最近の更新」から自動生成の404ページを除外する
+// (filterは関数なのでYAMLに書けず、TSオーバーライドで渡す)
+componentRegistry.setOptionOverrides("@quartz-community/recent-notes", {
+  filter: (f: { slug?: string }) => f.slug !== "404",
+})
 
 const config = await loadQuartzConfig()
 export default config
